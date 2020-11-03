73ee484b1bc67abf0c64865322ca5e8a3980ebe0
rupeshg98 <gudipudirupesh@gmail.com>
2020-11-03 14:39:20 -0500

    made Controller class

---
+++ Snapster-P2/src/main/java/com/revature/controller/SnapsterController.java
@@ -0,0 +1,13 @@
+package com.revature.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.service.SnapsterService;
+
+@RestController("snapsterController")
+public class SnapsterController {
+
+	private SnapsterService snapsterService;
+	
+	
+}
