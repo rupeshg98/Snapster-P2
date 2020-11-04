f484996513863033596d9ba617fbeebcd7ffde47
rupeshg98 <gudipudirupesh@gmail.com>
2020-11-04 00:30:26 -0500

    changed cross origin

--- my-app/src/main.ts
+++ my-app/src/main.ts
@@ -1,4 +1,6 @@
 import { enableProdMode } from '@angular/core';
+
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -10,3 +12,5 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
