a524dab7d3ac282295c76fdebd4dc52414f29998
rupeshg98 <gudipudirupesh@gmail.com>
2020-11-06 14:46:03 -0500

    added @component

--- Snapster-P2/src/main/java/com/revature/service/SnapsterService.java
+++ Snapster-P2/src/main/java/com/revature/service/SnapsterService.java
@@ -3,6 +3,7 @@ package com.revature.service;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.revature.model.FriendRequest;
@@ -12,6 +13,7 @@ import com.revature.repository.Snapster;
 import com.revature.repository.SnapsterImpl;
 
 @Service("SnapsterService")
+@Component
 public class SnapsterService {
 	
 	private Snapster snapsterRepo;
