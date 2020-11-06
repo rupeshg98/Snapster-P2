4a4c6e9408acb39498b8b286dadd82d208695150
rupeshg98 <gudipudirupesh@gmail.com>
2020-11-06 17:57:36 -0500

    post to get

--- Snapster-P2/src/main/java/com/revature/controller/SnapsterController.java
+++ Snapster-P2/src/main/java/com/revature/controller/SnapsterController.java
@@ -2,8 +2,9 @@ package com.revature.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.service.SnapsterService;
@@ -11,11 +12,12 @@ import com.revature.service.SnapsterService;
 @RestController(value = "SnapsterController")
 
 @CrossOrigin(origins = "http://localhost:4200", allowedHeaders="*")
+@RequestMapping(path = "/validateLogin")
 public class SnapsterController {
 	
 	SnapsterService snapsterService;
 
-	@PostMapping(path = "/validateLogin", produces =MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path = "/login", produces =MediaType.APPLICATION_JSON_VALUE)
 	public void validateLogin(@RequestBody String username, @RequestBody String password) {
 		System.out.println("SnapsterController Received Username " + username + " , Password: " + password);
 		
