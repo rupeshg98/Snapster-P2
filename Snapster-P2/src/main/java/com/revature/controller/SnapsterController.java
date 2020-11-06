d14e0e30afaf8a022f243dc8f477ef0c9ce80ca6
rupeshg98 <gudipudirupesh@gmail.com>
2020-11-06 15:16:08 -0500

    small convention changes

--- Snapster-P2/src/main/java/com/revature/controller/SnapsterController.java
+++ Snapster-P2/src/main/java/com/revature/controller/SnapsterController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.service.SnapsterService;
 
-@RestController(value = "snapsterController")
+@RestController(value = "SnapsterController")
 
 @CrossOrigin(origins = {"*"})
 public class SnapsterController {
