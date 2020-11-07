b4b97b27e29fb6f36de32a97a7096e173496144c
rupeshg98 <gudipudirupesh@gmail.com>
2020-11-06 22:43:57 -0500

    added autowired

--- Snapster-P2/src/main/java/com/revature/controller/SnapsterController.java
+++ Snapster-P2/src/main/java/com/revature/controller/SnapsterController.java
@@ -1,9 +1,9 @@
 package com.revature.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +14,8 @@ import com.revature.service.SnapsterService;
 @CrossOrigin(origins = {"http://localhost:4200"}, allowedHeaders="*")
 
 public class SnapsterController {
-	
-	SnapsterService snapsterService = new SnapsterService();
+	@Autowired
+	SnapsterService snapsterService;// = new SnapsterService();
 
 	@GetMapping(path = "/login", produces=MediaType.APPLICATION_JSON_VALUE)
 	public String validateLogin(@RequestParam("username") String username, @RequestParam("password") String password) {
--- Snapster-P2/src/main/java/com/revature/service/SnapsterService.java
+++ Snapster-P2/src/main/java/com/revature/service/SnapsterService.java
@@ -3,7 +3,6 @@ package com.revature.service;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.revature.model.FriendRequest;
